b978079828e384ba4ebbde87a23a5d937bb157d8
Lane <lanefox@gmail.com>
2022-09-08 10:35:00 -0500

    .

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# AssetVerifierDiscordBot
-This is a bot you can run on Discord to verify invoices from users when they send their invoice or order number via DM to the bot.
+# Asset Store Invoice Verifier Bot
+This is a Discord bot that will verify Invoice Numbers from the Unity Asset Store API and assign a role to the user submitting the invoice for verification.
 
-You need to host the code on replit, and use uptimerobot to ping it so it stays online and replit wont auto-close it after x hours.
+![The bot help response](help.png?raw=true "Help")
 
-When a user sends a DM to the bot with a valid invoice number it will assign it a role in your server. You can manage permissions of that role to restrict access to specific channels, or support channels altogether. Whatever you see fit to do with it.
+# Functional Process
+1. A user in your server DMs the bot an invoice number.
+2. The bot uses the UAS web API to verify it.
+3. If it contains a product name that matches something in the hard-coded map then it assigns the corresponding Role and reports the action to the discord report channel.
+4. User now has XYZ role(s), and can see channels restricted to those roles.
 
-More credits, details and comments are in the source code.
+# Base Requirements
+* Create a Application/Bot through Discord's developer dashboard.
+* Assign your Secret Discord Bot Token in Heroku Settings
+* Assign your Secret UAS API Token in Heroku Settings
+* Modify the code to suit your server channel IDs role IDs and asset names.
+
+# Heroku Setup
+Currently I am running this on Heroku using a worker dyno. If you have a CC on file with Heroku then you get 1,000h/mo for free which covers the bot entirely. Below is the video I used to figure out how to set it up. You can hook Heroku into GitHub directly to auto-update when you issue commits to your bot repo.
+[![Heroku Setup Tutorial](https://img.youtube.com/vi/OFearuMjI4s/0.jpg)](https://www.youtube.com/watch?v=OFearuMjI4s)
+
+# Discord Setup
+You need to setup a unique bot for yourself in the Discord application dashboard.
+https://discord.com/developers/applications
+
+You will also need to get the Channel IDs, the Role IDs and Guild ID from your Discord desktop application. You can do this by putting Discord into Developer mode (Settings > Advanced) and getting access to new right-click options in the app to fetch IDs needed for your bot (Dont use the included ones, those are IDs to my servers stuff and wont work for you).
+
+# Repl.it alternative
+You can host this on other services such as Repl.it but you need to use a service like uptimerobot and run an express app inside the bot to ping the app every few minutes to prevent the host from sleeping it. This was the original way the bot was designed, but repl.it things got buggy after they started to monetize always-on features.
--- index.js
+++ index.js
@@ -1,214 +1,187 @@
-// ===================
-// Informational Links
-// ===================
-//
-// === Setup up Discord App and Bot + Set UptimeRobot
-// https://youtu.be/SPTfmiYiuok?t=113
-// https://youtu.be/SPTfmiYiuok?t=3817
-//
-// === DiscordAPIError: Missing Permission
-// https://support.glitch.com/t/discordapierror-missing-permission/12519/2
-//
-// === Eric's original bot code
-// https://repl.it/@sonicbloomeric/Unity-AS-Invoice-Verification-Bot#index.js
-//
-// === Discord Server ID
-// To get Guild ID and Channel IDs, put discord into Developer mode and right click the server/channel to reveal "Copy ID".
-//
-// === UAS Store API Key & Docs
-// https://publisher.assetstore.unity3d.com/verify-invoice.html
-// https://api.assetstore.unity3d.com/api-docs/#!/invoice
-
-const discord = require("discord.js");
-const express = require("express");
-const fs = require("fs");
-const https = require("https");
-
-// ===================
-// = EXPRESS/DISCORD =
-// ===================
-const app = express();
-app.get("/", (req, res) => {res.send("Still alive!");});
-app.listen(3000, () => console.log("Started Listening..."));
-const client = new discord.Client();
-
-// Credentials/Secrets stored as environment variables. See: https://docs.repl.it/repls/secret-keys
-const DISCORD_TOKEN               = process.env.DISCORD_TOKEN;              // Discord API Token - this lets this code log into Discord as the Bot.
-const DISCORD_GUILD_ID            = process.env.DISCORD_GUILD_ID;           // Discord Guild (Server) ID - this is your Discord server ID.
-const DISCORD_REPORT_CHANNEL_ID   = process.env.DISCORD_REPORT_CHANNEL_ID;  // Discord Channel for Verified Reports - "Copy ID" on the private channel.
-const UAS_HOST                    = "api.assetstore.unity3d.com";           // Asset Store API Domain (never changes)
-const UAS_PATH                    = process.env.UAS_PATH;                   // Asset Store API Endpoint (see below)
-const ROLE_NAME                   = process.env.ROLE_NAME;                  // The name of the role to assign when invoice is verified
-
-// Example: (your store api key is mashed in here. we slap the invoice number on the end to ship it to Unity.)
-// UAS_PATH=/publisher/v1/invoice/verify.json?key=**__STORE_API_KEY__**&invoice=
+// UAS
+const UAS_HOST = "api.assetstore.unity3d.com";
+const UAS_PATH = "/publisher/v1/invoice/verify.json?key="+process.env.UAS_TOKEN+"&invoice=";
+// Discord
+const DISCORD_GUILD_ID      = "504309807876931586";
+const DISCORD_REPORT_CHANNEL_ID = "816347143425491005";
+const ROLE_ID_VERIFIED      = "796799179930206259";
+const ROLE_ID_VCORE         = "933411053700792460";
+const ROLE_ID_VATTRIBUTES   = "933410927938781304";
+const ROLE_ID_VINVENTORY    = "933411084885450752";
+const ROLE_ID_PRODIGY       = "933411165055361044";
+const ROLE_ID_THATHURT      = "933417015476121600";
+const ROLE_ID_QUINN         = "933411189369745448";
+const ROLE_ID_ZED           = "933416260362977280";
+const ROLE_ID_STYLIZED      = "933416171674431528";
+const ROLE_ID_DEFTLY        = "933416063310368818";
+const HELPMESSAGE           = "I can verify your purchase if you send me a message with _only_ the order/invoice id. If you include anything else in the message Ill be confused!\n\nAsset Store _Order_ IDs look like a big string of numbers: **1301234567890**\nAsset Store _Invoice_ IDs look like numbers but start with IN: **IN1234567890**\nYou can find your orders by going to the asset store, clicking your profile, then clicking My Orders, or click the link below.\n\nhttps://assetstore.unity.com/orders";
 
 // Map of Product Name to Discord Server Role name.
 // Add custom role assignments for products here.
 const assetToRoleMap = new Map();
-assetToRoleMap.set("Vault Core", ROLE_NAME);
-assetToRoleMap.set("Vault Inventory", ROLE_NAME);
-assetToRoleMap.set("Vault Inventory (fill-your-toolbox)", ROLE_NAME);
-assetToRoleMap.set("Vault Attributes", ROLE_NAME);
-assetToRoleMap.set("Prodigy", ROLE_NAME);
-assetToRoleMap.set("Zed", ROLE_NAME);
-assetToRoleMap.set("That Hurt! Damage Floaties", ROLE_NAME);
-assetToRoleMap.set("Deftly: Top Down Shooter Framework", ROLE_NAME);
-assetToRoleMap.set("Quinn Stylized Animset", ROLE_NAME);
-assetToRoleMap.set("Stylized Fantasy Character Pack", ROLE_NAME);
+assetToRoleMap.set("Vault Core",                            ROLE_ID_VCORE);
+assetToRoleMap.set("Vault Inventory",                       ROLE_ID_VINVENTORY);
+assetToRoleMap.set("Vault Inventory (fill-your-toolbox)",   ROLE_ID_VINVENTORY); // Official sales often have unique product names for them.
+assetToRoleMap.set("Vault Attributes",                      ROLE_ID_VATTRIBUTES);
+assetToRoleMap.set("Prodigy Game Framework",                ROLE_ID_PRODIGY);
+assetToRoleMap.set("That Hurt! Damage Floaties",            ROLE_ID_THATHURT);
+assetToRoleMap.set("Zed",                                   ROLE_ID_ZED);
+assetToRoleMap.set("Deftly: Top Down Shooter Framework",    ROLE_ID_DEFTLY);
+assetToRoleMap.set("Quinn Stylized Animset",                ROLE_ID_QUINN);
+assetToRoleMap.set("Stylized Fantasy Character Pack",       ROLE_ID_STYLIZED);
 
-client.on("ready", () => {console.log("Connected!");});
-client.on("message", message => 
-{
-    // Early fail options.
-    if (message.author.bot) return;
-    if (message.channel.type != "dm") return;
+const https = require("https");
 
-    // Disable gracefully. (when true)
-    if (false)
-    {
-      message.channel.send(`I'm currently offline! A human will get me up and running soon. Sorry for the inconvenience!`);
-      return;
-    }
-
-    // ======================
-    // PARSE THE MESSAGE FROM THE USER
-    // ======================
-    if (message.cleanContent.length > 15) // UAS API responds 500 if you give it more than 15 characters. Will this be forever? Who knows. Could we handle this better? Yep.
-    {
-      message.channel.send(`... _${message.cleanContent}_ does not look like a valid invoice or order id. Here is some help:`);
-      respondWithHelp(message);
-      return;
-    }
-    if (/^\d+$/.test(message.cleanContent)) // UAS orders are always complete numbers. test for it.
-    {
-      message.channel.send(`... Looks like an Asset Store _order id_ ! I'm looking into it... (_${message.cleanContent}_)`);
-      validateAssetStore(message);
-      return;
-    }
-    else if (message.cleanContent.startsWith('IN')) // UAS invoice numbers start with "IN". Not all orders have Invoice IDs
-    {
-      message.channel.send(`... Looks like an Asset Store _invoice id_ ! I'm looking into it... (_${message.cleanContent}_)`);
-      validateAssetStore(message);
-      return;
-    }
-    else if (message.cleanContent.includes('help')) // maybe they're confused.
-    {
-      respondWithHelp(message);
-      return;
-    }
-    else // gibberish? if you use FastSpring you'll validate that somewhere in here, I presume.
-    {
-      message.channel.send(`I don't understand! Here is some help:`);
-      respondWithHelp(message);
-      return;
-    }
+// Setup Discord Client (^14.3)
+// Not all of these are required. Partials.Channel gives access to DMs (I have no idea why).
+// Intents is the biggest problem when updating discord.js because they have provide confusing support layer.
+// I think you can just send the actual bit, but the online bit calculators fot that are built for various versions so results may vary.
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ],
+    partials: [Partials.Channel]
 });
+client.on("ready", () => {
+    console.log("Logged into Discord!");
+    const guild = client.guilds.cache.get(DISCORD_GUILD_ID);
+    guild.channels.cache.get(DISCORD_REPORT_CHANNEL_ID)
+        .send(`Bot has rebooted. [${new Date()}].`)
+        .catch(() => console.log("Ready message to report channel failed to send!"));
+});
+client.on("messageCreate",
+    message => {
+        // Ignore anything sent to the verify report channel
+        if (message.channel.id === DISCORD_REPORT_CHANNEL_ID) { return; }
+        // Ignore anything sent by bots (such as itself)
+        if (message.author.bot) { return; }
+        // This seems to change between discord versions and can be a hassle to debug.
+        if (message.channel.type !== 1 && message.channel.type.toString() !== "DM" && message.channel.type.toString() !== "dm") { return; }
 
-function respondWithHelp(message)
-{
-  message.channel.send(`
-  I can verify your purchase if you send me a message with _only_ the order/invoice id. If you include anything else in the message I'll be confused!
-  
-  Asset Store _Order_ IDs look like a big string of numbers: **1301234567890**
-  Asset Store _Invoice_ IDs look like numbers but start with "IN": **IN1234567890**
-  You can find your orders by going to the asset store, clicking your profile, then clicking My Orders, or click the link below.
-  
-  https://assetstore.unity.com/orders`);
-  return;
-}
-
-function validateAssetStore(message)
-{
-  // Guild object for use throughout.
-  const guild = client.guilds.cache.get(DISCORD_GUILD_ID);
-  try
-  {
-    const memberPromise = guild.members.fetch(message.author.id);
-    memberPromise.then(member => 
-    {
-      const invoiceID = message.cleanContent;
-      console.log(`Received message with contents: ${invoiceID}`);
-      const options = 
-      {
-        host: UAS_HOST,
-        path: UAS_PATH + invoiceID,
-        method: "GET",
-      };
-
-      console.log(`Verifying invoice at: ${UAS_HOST + UAS_PATH + invoiceID}`);
-
-      const req = https.request(options, res => 
-      {
-        console.log(`Invoice verification response status: ${res.statusCode}`);
-        res.setEncoding("utf8");
-
-        // TODO: Handle multi-part data packets? Does this ever happen?
-        res.on("data", chunk => 
+        const guild = client.guilds.cache.get(DISCORD_GUILD_ID);
+        const memberPromise = guild.members.fetch(message.author.id);
+        memberPromise.then(member =>
         {
-          const data = JSON.parse(chunk);
-
-          // For JSON format info, see: https://api.assetstore.unity3d.com/api-docs/#!/invoice
-          if (Array.isArray(data.invoices) && data.invoices.length > 0)
-          {
-            const purchaseReports = [];
-            for (const invoice of data.invoices)
+            // ======================
+            // PARSE THE MESSAGE FROM THE USER
+            // ======================
+            if (message.cleanContent.length > 15)
             {
-              const roleToAdd = assetToRoleMap.get(invoice.package);
-              if (roleToAdd !== undefined)
-              {
-                const role = guild.roles.cache.find(role => role.name === roleToAdd);
-                member.roles.add(role);
-                message.channel.send(`I verified your _${invoice.package}_ purchase and assigned you the **${roleToAdd}** role!`);
-
-                // For reporting purposes.
-                purchaseReports.push(
-                {
-                  invoice: invoice,
-                  role: roleToAdd,
-                });
-              }
+                message.channel
+                    .send(`... _${message.cleanContent}_ does not look like a valid invoice or order id. Here is some help:`)
+                    .catch(err => console.error(err.message));
+                message.channel.send(HELPMESSAGE).catch(err => console.error(err.message));
+                return;
+            } else if (message.cleanContent.includes("help"))
+            {
+                message.channel.send(HELPMESSAGE).catch(err => console.error(err.message));
+                return;
             }
 
-            // In case none of the packages we care about were found.
-            if (purchaseReports.length === 0)
+            if (/^\d+$/.test(message.cleanContent) || message.cleanContent.startsWith("IN")) {
+                message.channel.send(`Thanks, I'm looking into it! (id _${message.cleanContent}_)`)
+                    .catch(err => console.error(err.message));
+                console.log(`INPUT from ${member.displayName}`);
+
+                try {
+                    const invoiceId = message.cleanContent;
+                    const options =
+                    {
+                        host: UAS_HOST,
+                        path: UAS_PATH + invoiceId,
+                        method: "GET"
+                    };
+
+                    console.log(`Verifying '${invoiceId}'`);
+
+                    const req = https.request(options,
+                        res => {
+                            console.log(`Response: ${res.statusCode}`);
+                            res.setEncoding("utf8");
+                            res.on("data",
+                                chunk => {
+                                    const data = JSON.parse(chunk);
+                                    if (Array.isArray(data.invoices) && data.invoices.length > 0)
+                                    {
+                                        const purchaseReports = [];
+
+                                        for (const invoice of data.invoices)
+                                        {
+                                            const roleIdToAdd = assetToRoleMap.get(invoice.package);
+                                            const role = guild.roles.cache.get(roleIdToAdd);
+                                            const verifiedRole = guild.roles.cache.get(ROLE_ID_VERIFIED);
+
+                                            if (roleIdToAdd !== undefined)
+                                            {
+                                                // give verified role and special role
+                                                member.roles.add(role);
+                                                member.roles.add(verifiedRole);
+                                                message.channel
+                                                    .send(`I verified your _${invoice.package}_ purchase and assigned you the **${role.name}** role! You can verify other invoices for more roles.`)
+                                                    .catch(err => console.error(err.message));
+
+                                                // For reporting purposes.
+                                                purchaseReports.push(
+                                                    {
+                                                        invoice: invoice,
+                                                        role: roleIdToAdd
+                                                    });
+                                            }
+                                        }
+
+                                        // In case none of the packages we care about were found.
+                                        if (purchaseReports.length === 0)
+                                        {
+                                            message.channel
+                                                .send(`Hmm... I don't see any verifiable products in there. Are you sure that's the right order? If the product was in an official sale or is brand new, message an Admin for help.`)
+                                                .catch(err => console.error(err.message));
+                                        }
+                                        else
+                                        {
+                                            // Report the purchase to the invoice reporting channel.
+                                            for (const report of purchaseReports)
+                                            {
+                                                guild.channels.cache.get(DISCORD_REPORT_CHANNEL_ID).send(`Verified **${member}** for their purchase of _${report.invoice.package}_ (${report.invoice.quantity} seat${(report.invoice.quantity > 1 ? "s" : "")}). Invoice#: [${report.invoice.invoice}] (UAS).`);
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        message.channel
+                                            .send(`That number (${invoiceId}) doesn't seem valid or doesn't contain any of our products. Please double check it and try again.\nIf the problem persists, message a human with the Admin role.`)
+                                            .catch(err => console.error(err.message));
+                                    }
+                                });
+                        });
+
+                    // Handle the error case.
+                    req.on("error",
+                        error => {
+                            console.error(error);
+                            message.channel
+                                .send("Sorry, my connection failed... Try again later or message a human with the Admin role.")
+                                .catch(err => console.error(err.message));
+                        });
+                    req.end();
+                } catch (e) {
+                    console.error(e);
+                    message.channel.send("I ran into an error. Have you joined the Discord server?")
+                        .catch(err => console.error(err.message));
+                    return;
+                }
+            } else
             {
-              // This should basically never happen, you probably have all valid products mapped to at least one role
-              // so the error basically suggests 'yes, this order has our products because the API let us see it, but theres nothing we verify via the bot here.'
-              message.channel.send(`Hmm... I don't see any verifiable products in there. Are you sure that's the right order?`);
+                message.channel.send(`I don't understand! Here is some help:`).catch(err => console.error(err.message));
+                message.channel.send(HELPMESSAGE).catch(err => console.error(err.message));
+                return;
             }
-            else
-            {
-              // Report the purchase to the invoice reporting channel.
-              const reportChannel = guild.channels.cache.find(channel => channel.id === DISCORD_REPORT_CHANNEL_ID);
-              for (const report of purchaseReports)
-              {
-                reportChannel.send(`Verified **${member.displayName}** for their purchase of _${report.invoice.package}_ (${report.invoice.quantity} seat${(report.invoice.quantity > 1 ? "s" : "")}). Invoice#: [${report.invoice.invoice}] (UAS).`);
-              }
-            }
-          }
-          else
-          {
-            message.channel.send(`That number (${invoiceID}) doesn't seem valid or doesn't contain any of our products. Please double check it and try again.\nIf the problem persists, message a human with the Admin role.`);
-          }
         });
-      });
-
-      // Handle the error case.
-      req.on("error", error => 
-      {
-        console.error(error);
-        message.channel.send("Sorry, my connection failed... Try again later or message a human with the Admin role.");
-      });
-      req.end();
     });
-  }
-  catch (e)
-  {
-    console.error(e);
-    message.channel.send("I ran into an error. Have you joined the Discord server?");
-    return;
-  }
-}
 
-client.login(DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
